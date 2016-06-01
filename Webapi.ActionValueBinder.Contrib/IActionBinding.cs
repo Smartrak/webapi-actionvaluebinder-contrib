c0c8fdb3b9cef8f0f79d090242ec29620548e9a8
lloydstockman <ljstockman23@hotmail.com>
2016-06-01 13:27:09 +1200

    initial commit which has  a class that can be used to do multiple bindings instead of a single one, however if multiple bindings can be used only the first one will get chosen.

---
+++ Webapi.ActionValueBinder.Contrib/IActionBinding.cs
@@ -0,0 +1,10 @@
+﻿using System.Web.Http.Controllers;
+
+namespace Webapi.ActionValueBinder.Contrib
+{
+	public interface IActionBinding
+	{
+		bool CanBind(HttpParameterDescriptor parameter);
+		HttpParameterBinding Bind(HttpParameterDescriptor parameter);
+	}
+}
---
+++ Webapi.ActionValueBinder.Contrib/MultiActionBinder.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http.Controllers;
+using System.Web.Http.ModelBinding;
+
+namespace Webapi.ActionValueBinder.Contrib
+{
+    public class MultiActionBinder : DefaultActionValueBinder
+    {
+	    private List<IActionBinding> _bindings;
+
+	    public MultiActionBinder()
+	    {
+		    _bindings = new List<IActionBinding>();
+	    } 
+		
+		protected override HttpParameterBinding GetParameterBinding(HttpParameterDescriptor parameter)
+		{
+			var possibleBindings = _bindings.Where(x => x.CanBind(parameter)).ToArray();
+			if (possibleBindings.Any())
+				return possibleBindings.First().Bind(parameter);
+
+			return base.GetParameterBinding(parameter);
+		}
+
+		public void AddBinding(IActionBinding binding)
+	    {
+		    _bindings.Add(binding);
+	    }
+    }
+}
